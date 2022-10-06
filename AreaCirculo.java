d91e0103e6b0ed01bff1af2db82d51237555ab7c
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado@users.noreply.github.com>
2022-10-05 23:43:22 -0300

    Add files via upload

---
+++ AreaCirculo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math. *;
+public class AreaCirculo {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o raio do círculo:");
+        double raio = Double.parseDouble(scanner.next());
+        double area = (raio*raio)*Math.PI;
+        System.out.println("A área do círculo é igual a: "+area+".");
+    }
+}
